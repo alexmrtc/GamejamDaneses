638e9bc6d32ebc2e2e48308a0c3f34eb1c94261f
Alex <alexmrtc@gmail.com>
2019-10-23 13:35:50 +0200

    Menus_v.0.1

--- Gamejam/Assets/Alex/MainMenu.cs
+++ Gamejam/Assets/Alex/MainMenu.cs
@@ -1,25 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Cursor.visible = true --> Para hacer que el cursor sea visible
         
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Quit()
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void ShowCreditsHideMenu()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void ShowMenuHideCredits()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
