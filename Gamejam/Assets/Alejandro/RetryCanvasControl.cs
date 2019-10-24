443bc321373bf1e75c4d1127729f68dd7a3fef46
Narukyn <alexmusu99@gmail.com>
2019-10-24 14:49:20 +0200

    Triggers and Canvas

---
+++ Gamejam/Assets/Alejandro/RetryCanvasControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RetryCanvasControl : MonoBehaviour
+{
+    public GameObject canvasRetry;
+    //public Transform spawnPosition;
+    //private Quaternion spawnQuaternion;
+    public collision character;
+    
+
+    public void yesButton()
+    {
+        canvasRetry.SetActive(false);
+
+        //Instantiate(character, spawnPosition.position, spawnQuaternion);
+        //Instantiate(character, spawnPosition);
+
+        character.resetCharacter();
+    }
+
+    public void noButton()
+    {
+        canvasRetry.SetActive(false);
+        
+        // Load main scene (title scene)
+
+    }
+}
--- Gamejam/Assets/Alejandro/collision.cs
+++ Gamejam/Assets/Alejandro/collision.cs
@@ -4,33 +4,85 @@ using UnityEngine;
 
 public class collision : MonoBehaviour
 {
+    #region variables
     public Collider2D characterCollider;
+    public GameObject[] souls;
+    public GameObject canvasRetry;
+    public Transform spawnPoint;
 
-    // Start is called before the first frame update
-    
-    // Update is called once per frame
-    void Update()
-    {
+    private int soulsEaten;
+    #endregion
+
+    private void Start() {
+        // Starts the player
+        this.enabled = true;
         
+        // Restarts the count souls.
+        soulsEaten = 0;
+
+        // Actives again all the souls so they can be eaten again.
+        for (int i = 0; i < souls.Length; i++)
+        {
+            souls[i].SetActive(true);
+        }
+    }
+
+    public void resetCharacter()
+    {
+        // Sets the position
+        this.transform.position = spawnPoint.position;
+
+        // Starts the player
+        this.enabled = true;
+
+        // Restarts the count souls.
+        soulsEaten = 0;
+
+        // Actives again all the souls so they can be eaten again.
+        for (int i = 0; i < souls.Length; i++)
+        {
+            souls[i].SetActive(true);
+        }
+    }
+
+    private void spawnRetryCanvas()
+    {
+
+        canvasRetry.SetActive(true);
+    }
+
+    private int getSoulEated(Collider2D trigger) {
+        int retVal = 0;
+
+        retVal = int.Parse(trigger.name.Split("("[0])[1].Split(")"[0])[0]);
+
+        return retVal;
     }
 
     public void OnTriggerEnter2D(Collider2D trigger)
     {
-        if (trigger.name == "death")
+        if (trigger.name == "TriggerDeath")
         {
-            // @TODO: death things
+            //Destroy(this.gameObject);
+            this.enabled = false;
+            spawnRetryCanvas();
         }
-        else if (trigger.name == "clickable zone")
+        else if (trigger.name == "TriggerClickableZone")
         {
             // @TODO: click and drag things
         }
-        else if (trigger.name == "soul")
-        {
-            // @TODO: eating souls things
-        }
         else
         {
-            Debug.Log("Trigger ok");
+            soulsEaten++;
+
+            int n_soulEaten = getSoulEated(trigger);
+
+            //Debug.Log(n_soulEaten);
+
+            // There's a -1 so we can start counting in the viewport from 1 and not 0 (easier for artists I think).
+            // Uncomment the line after when the souls have been placed in map and inspector.
+            
+            //souls[n_soulEaten - 1].SetActive(false);
         }
     }
 }
