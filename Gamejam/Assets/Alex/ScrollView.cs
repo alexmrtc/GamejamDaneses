43252fb1646e4a1eca0b5df2fad401909e13275c
Alex <alexmrtc@gmail.com>
2019-10-25 10:15:46 +0200

    Background

---
+++ Gamejam/Assets/Alex/ScrollView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollView : MonoBehaviour
+{
+    public GameObject character;
+    public Material material;
+    public bool moving;
+    public float offsetX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving == true)
+        {
+            material.SetTextureOffset() + offsetX;
+        }
+    }
+}
